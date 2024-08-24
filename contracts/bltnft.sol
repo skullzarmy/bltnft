203a3929d4aa264b2431b187835f15d11312572e
skllzrmy.tez <joe@poundfit.com>
2024-08-24 11:55:26 -0700

    Initial commit from npx thirdweb create

---
+++ contracts/bltnft.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+
+contract bltnft is ERC1155LazyMint {
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    ) ERC1155LazyMint(_defaultAdmin, _name, _symbol, _royaltyRecipient, _royaltyBps) {}
+}
